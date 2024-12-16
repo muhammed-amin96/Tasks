d2f4975e0257c0bfc81562324935c8657aed723c
Muhammad Amin <mohamedamin.fcih@gmail.com>
2024-12-16 17:18:26 +0200

    initialized project

---
+++ total-cost-of-grocery-items/bin/total_cost_of_grocery_items.dart
@@ -0,0 +1,5 @@
+import 'package:total_cost_of_grocery_items/total_cost_of_grocery_items.dart' as total_cost_of_grocery_items;
+
+void main(List<String> arguments) {
+  print('Hello world: ${total_cost_of_grocery_items.calculate()}!');
+}
---
+++ total-cost-of-grocery-items/test/total_cost_of_grocery_items_test.dart
@@ -0,0 +1,8 @@
+import 'package:total_cost_of_grocery_items/total_cost_of_grocery_items.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
